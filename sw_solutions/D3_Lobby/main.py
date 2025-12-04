48c3337bb9a39e5f08f6f18917159e99b7fefac6
abhijay-py <jay62831@gmail.com>
2025-12-04 16:52:12 -0500

    finished problem 3

---
+++ sw_solutions/D3_Lobby/main.py
@@ -0,0 +1,50 @@
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+def get_data():
+    with open(os.path.join(here, "input.txt")) as f:
+        data = f.readlines()
+    
+    data_clean = [entry.strip() for entry in data]
+
+    return data_clean
+
+def part_one():
+    data = get_data()
+    total_joltage = 0
+    for line in data:
+        max_one = -1
+        max_two = -1
+        for i in range(len(line)):
+            if int(line[i]) > max_one and i != len(line) - 1:
+                max_one = int(line[i])
+                max_two = -1
+            elif int(line[i]) > max_two:
+                max_two = int(line[i])
+        total_joltage += max_one * 10 + max_two
+    print(f"Total Two Digit Joltage: {total_joltage}")
+
+def part_two():
+    data = get_data()
+    total_joltage = 0
+    for line in data:
+        max_values = [-1 for i in range(12)]
+        for i in range(len(line)):
+            updated_max = False
+            for j in range(12):
+                if updated_max:
+                    max_values[j] = -1
+                elif int(line[i]) > max_values[j] and i <= len(line) - 12 + j:
+                    max_values[j] = int(line[i])
+                    updated_max = True
+        for i in range(12):
+            total_joltage += max_values[i] * (10 ** (11 - i))    
+    print(f"Total Twelve Digit Joltage: {total_joltage}")
+
+def main():
+    part_one()
+    part_two()
+    
+if __name__ == "__main__":
+    main()
